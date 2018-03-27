3f5ae615d0ec285f3e9f068baa7f7eab5e5ee311
andrew lupton and Eva Antipina <eva@>
2018-03-27 09:30:55 -0700

    add if/else conditions to account for negatives and NaN user inputs.

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,20 @@ $(document).ready(function() {
     var userCountTo = parseInt($("input#countTo").val());
     var userCountBy = parseInt($("input#countBy").val());
 
+    if (userCountBy > userCountTo) {
+      alert("Invalid response. The destination number must be larger than the multiple.");
+      return;
+    } else if (userCountBy === 0 || userCountBy < 0 || userCountTo < 0) {
+      alert("Invalid response. The multiple cannot be 0 and neither input can be negative.");
+      return;
+    } else if (isNaN(userCountTo) || isNaN(userCountBy)) {
+      alert("Invalid response. Your input is not a number!");
+      return;
+    } else if (!userCountBy || !userCountTo) {
+      alert("Invalid response. The box cannot be empty.");
+      return;
+    }
+
     var array = [];
     for (var i = 0; i <= userCountTo; i++) {
       array[i]=i;
